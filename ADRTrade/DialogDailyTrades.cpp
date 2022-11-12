68768384797b6eb063e92c5d57c8a30637f3786f
Ruffia <n02022407@hotmail.com>
2022-11-12 20:22:27 +0800

    加上关于交易的UI

--- ADRTrade/DialogDailyTrades.cpp
+++ ADRTrade/DialogDailyTrades.cpp
@@ -4,6 +4,7 @@ using namespace std;
 #include "DialogDailyTrades.h"
 #include "Factory.h"
 #include "DialogIDManager.h"
+#include "DialogPlaceHolder.h"
 #include "Util.h"
 #include "UIData.h"
 #include "Tools/CollectiveComponentProvider.h"
@@ -29,8 +30,7 @@ CDlgDailyTrades::~CDlgDailyTrades()
 
 void CDlgDailyTrades::_InitLayOut()
 {
-	return;
-	string sFileName = GetModulePath() + "/UI/DailyTrades.xml";
+	string sFileName = GetModulePath() + "/UI/" + m_sLayout;
 	if (!m_doc.load_file(sFileName.c_str())) 
 	{ //¼ÓÔØxmlÎÄ¼þ
 		return;
@@ -55,6 +55,7 @@ void CDlgDailyTrades::_InitLayOut()
 		CUIData data;
 		const string sName = node.attribute("Name").as_string("");
 		data.m_strUIClassName = node.attribute("ClassName").as_string("");
+		data.m_strLayout = node.attribute("Layout").as_string("");
 		data.m_nID = node.attribute("ID").as_int();
 		data.m_nLeft = node.attribute("Left").as_int();
 		data.m_nTop = node.attribute("Top").as_int();
@@ -71,6 +72,11 @@ void CDlgDailyTrades::_InitLayOut()
 		CDialog* pDlg = Factory<CDialog,string>::Instance().BuildProduct(data.m_strUIClassName);
 		const int nIDD = CDialogIDMgr::Instance().GetDialogResourceID(data.m_strUIClassName);
 		ASSERT(-1 != nIDD);
+		CDialogPlaceHolder* pHolder = dynamic_cast<CDialogPlaceHolder*>(pDlg);
+		if (pHolder)
+		{
+			pHolder->SetLayout(data.m_strLayout);
+		}
 		pDlg->Create(nIDD,&m_Tab);
 		pDlg->SetParent(&m_Tab);
 		pDlg->MoveWindow(&rcHold);
---
+++ ADRTrade/DialogDailyTrades_SingleTradeTrack.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "DialogPlaceHolder.h"
+#include "Factory.h"
+
+class CDialogDailyTrades_SingleTradeTrack :public CDialogPlaceHolder
+{
+public:
+	CDialogDailyTrades_SingleTradeTrack();
+	enum { IDD = IDD_DLG_Common };
+};
