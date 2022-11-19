371b57bef5e7169330a5d7d5f7cf3d94205b37c9
Ruffia <n02022407@hotmail.com>
2022-11-19 17:24:43 +0800

    去掉某些非必要代码

--- ADRTrade/ADRTradeDayDoc.cpp
+++ ADRTrade/ADRTradeDayDoc.cpp
@@ -78,62 +78,9 @@ void CADRTradeDayDoc::Serialize(CArchive& ar)
 	}
 }
 
-#ifdef SHARED_HANDLERS
 
-// 缩略图的支持
-void CADRTradeDayDoc::OnDrawThumbnail(CDC& dc, LPRECT lprcBounds)
-{
-	// 修改此代码以绘制文档数据
-	dc.FillSolidRect(lprcBounds, RGB(255, 255, 255));
-
-	CString strText = _T("TODO: implement thumbnail drawing here");
-	LOGFONT lf;
-
-	CFont* pDefaultGUIFont = CFont::FromHandle((HFONT) GetStockObject(DEFAULT_GUI_FONT));
-	pDefaultGUIFont->GetLogFont(&lf);
-	lf.lfHeight = 36;
-
-	CFont fontDraw;
-	fontDraw.CreateFontIndirect(&lf);
-
-	CFont* pOldFont = dc.SelectObject(&fontDraw);
-	dc.DrawText(strText, lprcBounds, DT_CENTER | DT_WORDBREAK);
-	dc.SelectObject(pOldFont);
-}
-
-// 搜索处理程序的支持
-void CADRTradeDayDoc::InitializeSearchContent()
-{
-	CString strSearchContent;
-	// 从文档数据设置搜索内容。
-	// 内容部分应由“;”分隔
-
-	// 例如:  strSearchContent = _T("point;rectangle;circle;ole object;")；
-	SetSearchContent(strSearchContent);
-}
-
-void CADRTradeDayDoc::SetSearchContent(const CString& value)
-{
-	if (value.IsEmpty())
-	{
-		RemoveChunk(PKEY_Search_Contents.fmtid, PKEY_Search_Contents.pid);
-	}
-	else
-	{
-		CMFCFilterChunkValueImpl *pChunk = NULL;
-		ATLTRY(pChunk = new CMFCFilterChunkValueImpl);
-		if (pChunk != NULL)
-		{
-			pChunk->SetTextValue(PKEY_Search_Contents, value, CHUNK_TEXT);
-			SetChunkValue(pChunk);
-		}
-	}
-}
-
-#endif // SHARED_HANDLERS
 
 // CADRTradeDayDoc 诊断
-
 #ifdef _DEBUG
 void CADRTradeDayDoc::AssertValid() const
 {
