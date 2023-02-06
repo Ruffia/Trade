56105caa094525b510827c68ba47d237c4f9b862
Marco <n02022407@163.com>
2023-02-06 17:24:12 +0800

    SQL changes

--- Bin/SQL/FieldMeta.sql
+++ Bin/SQL/FieldMeta.sql
@@ -10,7 +10,7 @@
  Target Server Version : 3035005
  File Encoding         : 65001
 
- Date: 13/01/2023 17:17:27
+ Date: 06/02/2023 16:58:57
 */
 
 PRAGMA foreign_keys = false;
@@ -83,5 +83,38 @@ INSERT INTO "FieldMeta" VALUES ('GainLoss', 'Future_IntradayTrade', 'float', 0, 
 INSERT INTO "FieldMeta" VALUES ('HighestPrice', 'Future_IntradayTrade', 'float', 0, 1, 28);
 INSERT INTO "FieldMeta" VALUES ('LowestPricePrice', 'Future_IntradayTrade', 'float', 0, 1, 29);
 INSERT INTO "FieldMeta" VALUES ('TransactionQuality', 'Future_IntradayTrade', 'float', 0, 1, 30);
+INSERT INTO "FieldMeta" VALUES ('TradeDay', 'Future_DailyLongPeriodicAnalyze', 'string', 1, 0, 0);
+INSERT INTO "FieldMeta" VALUES ('FutureContractName', 'Future_DailyLongPeriodicAnalyze', 'string', 0, 1, 1);
+INSERT INTO "FieldMeta" VALUES ('MainForceChange', 'Future_DailyLongPeriodicAnalyze', 'string', 0, 1, 2);
+INSERT INTO "FieldMeta" VALUES ('CITIC_GoldmanSachs', 'Future_DailyLongPeriodicAnalyze', 'string', 0, 1, 3);
+INSERT INTO "FieldMeta" VALUES ('ExternalMarket', 'Future_DailyLongPeriodicAnalyze', 'string', 0, 1, 4);
+INSERT INTO "FieldMeta" VALUES ('MonthlyTrend', 'Future_DailyLongPeriodicAnalyze', 'string', 0, 1, 5);
+INSERT INTO "FieldMeta" VALUES ('WeeklyTrend', 'Future_DailyLongPeriodicAnalyze', 'string', 0, 1, 6);
+INSERT INTO "FieldMeta" VALUES ('DailyTrend', 'Future_DailyLongPeriodicAnalyze', 'string', 0, 1, 7);
+INSERT INTO "FieldMeta" VALUES ('4HTrend', 'Future_DailyLongPeriodicAnalyze', 'string', 0, 1, 8);
+INSERT INTO "FieldMeta" VALUES ('EmotionalFeeling', 'Future_DailyLongPeriodicAnalyze', 'string', 0, 1, 9);
+INSERT INTO "FieldMeta" VALUES ('TradeDay', 'Future_DailyMinorCycleAnalyze', 'string', 1, 0, 0);
+INSERT INTO "FieldMeta" VALUES ('FutureContractName', 'Future_DailyMinorCycleAnalyze', 'string', 0, 1, 1);
+INSERT INTO "FieldMeta" VALUES ('RecordTime', 'Future_DailyMinorCycleAnalyze', 'string', 0, 1, 2);
+INSERT INTO "FieldMeta" VALUES ('15MIN_MACD', 'Future_DailyMinorCycleAnalyze', 'string', 0, 1, 3);
+INSERT INTO "FieldMeta" VALUES ('1H_MACD', 'Future_DailyMinorCycleAnalyze', 'string', 0, 1, 4);
+INSERT INTO "FieldMeta" VALUES ('4H_MACD', 'Future_DailyMinorCycleAnalyze', 'string', 0, 1, 5);
+INSERT INTO "FieldMeta" VALUES ('15MIN_MACDPillar', 'Future_DailyMinorCycleAnalyze', 'string', 0, 1, 6);
+INSERT INTO "FieldMeta" VALUES ('1H_MACDPillar', 'Future_DailyMinorCycleAnalyze', 'string', 0, 1, 7);
+INSERT INTO "FieldMeta" VALUES ('4H_MACDPillar', 'Future_DailyMinorCycleAnalyze', 'string', 0, 1, 8);
+INSERT INTO "FieldMeta" VALUES ('15MIN_Trix', 'Future_DailyMinorCycleAnalyze', 'string', 0, 1, 9);
+INSERT INTO "FieldMeta" VALUES ('1H_Trix', 'Future_DailyMinorCycleAnalyze', 'string', 0, 1, 10);
+INSERT INTO "FieldMeta" VALUES ('4H_Trix', 'Future_DailyMinorCycleAnalyze', 'string', 0, 1, 11);
+INSERT INTO "FieldMeta" VALUES ('15MIN_SKDJ', 'Future_DailyMinorCycleAnalyze', 'string', 0, 1, 12);
+INSERT INTO "FieldMeta" VALUES ('1H_SKDJ', 'Future_DailyMinorCycleAnalyze', 'string', 0, 1, 13);
+INSERT INTO "FieldMeta" VALUES ('4H_SKDJ', 'Future_DailyMinorCycleAnalyze', 'string', 0, 1, 14);
+INSERT INTO "FieldMeta" VALUES ('15MIN_StrengthAngle', 'Future_DailyMinorCycleAnalyze', 'string', 0, 1, 15);
+INSERT INTO "FieldMeta" VALUES ('1H_StrengthAngle', 'Future_DailyMinorCycleAnalyze', 'string', 0, 1, 16);
+INSERT INTO "FieldMeta" VALUES ('4H_StrengthAngle', 'Future_DailyMinorCycleAnalyze', 'string', 0, 1, 17);
+INSERT INTO "FieldMeta" VALUES ('MainSignal', 'Future_DailyMinorCycleAnalyze', 'string', 0, 1, 18);
+INSERT INTO "FieldMeta" VALUES ('Inclination', 'Future_DailyMinorCycleAnalyze', 'string', 0, 1, 19);
+INSERT INTO "FieldMeta" VALUES ('EmotionFeeling', 'Future_DailyMinorCycleAnalyze', 'string', 0, 1, 20);
+INSERT INTO "FieldMeta" VALUES ('Forecast', 'Future_DailyMinorCycleAnalyze', 'string', 0, 1, 21);
+INSERT INTO "FieldMeta" VALUES ('Reality', 'Future_DailyMinorCycleAnalyze', 'string', 0, 1, 22);
 
 PRAGMA foreign_keys = true;
