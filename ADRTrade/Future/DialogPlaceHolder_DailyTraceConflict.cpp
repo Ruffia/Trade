8b348f3c924b16a0faaa503aff2b01f3e416fd18
Marco <n02022407@163.com>
2023-02-20 16:33:53 +0800

    新增Conflict 相关的类和UI文件

---
+++ ADRTrade/Future/DialogPlaceHolder_DailyTraceConflict.cpp
@@ -0,0 +1,441 @@
+#include "stdafx.h"
+#include "DialogPlaceHolder_DailyTraceConflict.h"
+#include "../Common/Factory.h"
+#include "DBDataManager.h"
+#include "Util.h"
+#include "Tools/CollectiveComponentProvider.h"
+#include "Tools/BusinessEdit.h"
+#include "Tools/BusinessComboBox.h"
+#include "Tools/BusinessCheckBox.h"
+#include "Tools/EditTreeCtrlEx.h"
+#include "PrimaryKeyRule.h"
+#include "TradeDayPrimaryData.h"
+#include "UIData.h"
+#include "Tools/StyleManager.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+
+IMPLEMENT_FACTORY(CDialogPlaceHolder,CDialogFutureContract_DailyTraceConflict, string,"CDialogFutureContract_DailyTraceConflict")
+CDialogFutureContract_DailyTraceConflict::CDialogFutureContract_DailyTraceConflict(CWnd* pParent /*=NULL*/)
+	: CDialogPlaceHolder(pParent)
+{
+	CDialogIDMgr::Instance().Register("CDialogFutureContract_DailyTraceConflict",CDialogFutureContract_DailyTraceConflict::IDD); 
+}
+
+
+void CDialogFutureContract_DailyTraceConflict::_InitLayOut()
+{
+	string sFileName = GetModulePath() + "/UI/" + m_sLayout;
+	bool bRet = true;
+	if (!m_doc.load_file(sFileName.c_str())) 
+	{ //加载xml文件
+		return;
+	}
+
+	CRect rc;
+	GetClientRect(rc);
+	ClientToScreen(rc);
+
+	xml_node root = m_doc.child("root");  //根节点
+	xml_node nodeLayout = root.child("Layout");
+	xml_node node = nodeLayout.first_child();
+	while (!node.empty())
+	{
+		CUIData data;
+		const string sName = node.attribute("Name").as_string("");
+		data.m_strUIClassName = node.attribute("ClassName").as_string("");
+		data.m_nID = node.attribute("ID").as_int();
+		data.m_nLeft = node.attribute("Left").as_int();
+		data.m_nTop = node.attribute("Top").as_int();
+		data.m_nWidth = node.attribute("Width").as_int();
+		data.m_nHeight = node.attribute("Height").as_int();
+		const string sStyle = node.attribute("Style").as_string();
+		DWORD dwTotalStyle = CStyleMgr::Instance().GetStyle(sStyle);
+		const string sCaption = node.attribute("Caption").as_string();
+		CFont* pFont = CCollectiveComponentProvider::Instance().GetFont();
+        if (data.m_strUIClassName.find("CBusinessEdit") != string::npos)
+		{
+			CBusinessEdit* pEdit = new CBusinessEdit;
+			CRect rc(data.m_nLeft,data.m_nTop,data.m_nLeft + data.m_nWidth ,data.m_nTop + data.m_nHeight);
+			pEdit->Create(dwTotalStyle,rc,this,data.m_nID);	
+			pEdit->SetFont(pFont);
+			pEdit->ShowWindow(SW_SHOW);
+			pEdit->SetWindowText(sCaption.c_str());
+			const string& sBusiness = node.attribute("business").as_string();
+			pEdit->m_sBusinessField = sBusiness;
+			m_mapBusiness2Control[sName] = pEdit;
+			m_mapUIName2Wnd[sName] = pEdit;	
+		}
+		else if (data.m_strUIClassName.find("CEdit") != string::npos)
+		{
+			CEdit* pEdit = new CEdit;
+			CRect rc(data.m_nLeft,data.m_nTop,data.m_nLeft + data.m_nWidth ,data.m_nTop + data.m_nHeight);
+			pEdit->Create(dwTotalStyle,rc,this,data.m_nID);	
+			pEdit->SetFont(pFont);
+			pEdit->ShowWindow(SW_SHOW);
+			pEdit->SetWindowText(sCaption.c_str());
+			m_mapUIName2Wnd[sName] = pEdit;	
+		}
+		else if (data.m_strUIClassName.find("CBusinessComboBox") != string::npos)
+		{
+			CBusinessComboBox* pCombox = new CBusinessComboBox;
+			CRect rc(data.m_nLeft,data.m_nTop,data.m_nLeft + data.m_nWidth ,data.m_nTop + data.m_nHeight);
+			pCombox->Create(dwTotalStyle,rc,this,data.m_nID);	
+			xml_node nodeDropdownItem = node.child("Dropdown");
+			string sSubject = nodeDropdownItem.attribute("Subject").as_string("");
+
+			string sSQL = "select * from Dictionary_Field where Subject ";
+			sSQL += "='";
+			sSQL += sSubject;
+			sSQL += "'";
+			sSQL += " order by value asc";
+
+			CDataSet ds;
+			CDBDataManager::Instance().LoadData(sSQL,"Dictionary_Field",ds);
+
+			const int nRecordCount = ds.Size();
+			for (int i = 0; i < nRecordCount;i++)
+			{
+				CRecord* pRecord = ds[i];
+				if(!pRecord) continue;
+
+				CField* pFieldValue = pRecord->GetField("Value");
+				if(!pFieldValue) continue;
+				int nValue = pFieldValue->GetValueAsInt();
+
+				CField* pFieldTranslation = pRecord->GetField("Translation");
+				if(!pFieldTranslation) continue;
+				string sMeaning = pFieldTranslation->GetValueAsString();
+				pCombox->AddString(sMeaning.c_str());
+				pCombox->Add2Map(nValue,sMeaning);
+			}
+
+			const string& sBusiness = node.attribute("business").as_string();
+			pCombox->m_sBusinessField = sBusiness;
+
+			pCombox->SetFont(pFont);
+			pCombox->ShowWindow(SW_SHOW);
+			pCombox->SetWindowText(sCaption.c_str());
+			m_mapBusiness2Control[sName] = pCombox;
+			m_mapUIName2Wnd[sName] = pCombox;	
+		}
+		else if(data.m_strUIClassName.find("CBusinessCheckBox") != string::npos)
+		{
+			CBusinessCheckBox* pCheckBox = new CBusinessCheckBox;
+			CRect rc(data.m_nLeft,data.m_nTop,data.m_nLeft + data.m_nWidth ,data.m_nTop + data.m_nHeight);	
+			pCheckBox->Create(sCaption.c_str(), dwTotalStyle,rc,this, data.m_nID);
+			pCheckBox->SetFont(pFont);
+			const string& sBusiness = node.attribute("business").as_string();
+			pCheckBox->m_sBusinessField = sBusiness;
+			m_mapBusiness2Control[sName] = pCheckBox;
+			m_mapUIName2Wnd[sName] = pCheckBox;	
+		}
+		node = node.next_sibling();
+	}
+}
+
+
+void CDialogFutureContract_DailyTraceConflict::_LoadTradeDayData2UI()
+{
+	bool bExists = _CheckExistsTradeDayRecord();
+	if (!bExists) return;
+
+	const string strTradeDay = "TradeDay";
+	const string strFutureContractName = "FutureContractName";
+	const string strNumber = "number";
+
+	vector<CFieldDesc*> vFieldDesc;
+	CDBDataManager::Instance().GetFieldMetaData(m_sBusiness,vFieldDesc);
+
+	vector<CFieldDesc*> vPrimaryKey;
+	CDBDataManager::Instance().GetPrimaryKey(m_sBusiness,vPrimaryKey);
+
+	CFieldDesc* pTradeDayDesc = NULL;
+	CFieldDesc* pFutureContractNameDesc = NULL;
+	CFieldDesc* pNumberDesc = NULL;
+	for (int i = 0;i < vPrimaryKey.size();i++)
+	{
+		CFieldDesc* pFieldDesc = vPrimaryKey[i];
+		if (pFieldDesc && pFieldDesc->m_strFieldName.find(strTradeDay) != string::npos)
+		{
+			pTradeDayDesc = pFieldDesc;
+		}
+		else if (pFieldDesc && pFieldDesc->m_strFieldName.find(strFutureContractName) != string::npos)
+		{
+			pFutureContractNameDesc = pFieldDesc;
+		}
+		else if (pFieldDesc && pFieldDesc->m_strFieldName.find(strNumber) != string::npos)
+		{
+			pNumberDesc = pFieldDesc;
+		}
+	}
+
+	if(!pTradeDayDesc || !pFutureContractNameDesc || !pNumberDesc) return;
+
+	string sSQL = "select * from ";
+	sSQL += m_sBusiness;
+	sSQL += " where ";
+	sSQL += pTradeDayDesc->m_strFieldName;
+	sSQL += " = '";
+	sSQL += CTradeDayPrimaryData::Instance().m_strTradeDay;
+	sSQL += "'";
+	sSQL += " and ";
+	sSQL += pFutureContractNameDesc->m_strFieldName;
+	sSQL += " = '";
+	sSQL += CTradeDayPrimaryData::Instance().m_strFutureContractName;
+	sSQL += "'";
+
+	CDataSet ds;
+	CDBDataManager::Instance().LoadData(sSQL,m_sBusiness,ds);
+
+	UpdateDB2UI(ds);
+}
+
+
+void CDialogFutureContract_DailyTraceConflict::UpdateUI2DB()
+{
+	bool bExists = _CheckExistsTradeDayRecord();
+	if(!bExists) return;
+
+	map<string,CFieldDesc*>& mapTableName2FieldDesc = CDBDataManager::Instance().GetTableMeta(m_sBusiness);
+
+	string sSQL = "update ";
+	sSQL += m_sBusiness;
+	sSQL += " set ";
+
+	int nUIControlCount = 0;
+	for(map<string,CWnd*>::iterator it = m_mapBusiness2Control.begin();
+		it != m_mapBusiness2Control.end();it++)
+	{
+		CWnd* pWnd = it->second;
+		nUIControlCount++;
+		bool bComBox = false;
+		string sBusinessField = "";
+
+		//截至目前，有3中Business 字段类型
+		CBusinessEdit* pBusinessControl = NULL;
+		CBusinessComboBox* pComboBoxControl = NULL;
+		CBusinessCheckBox* pCheckBoxControl = NULL;
+
+		const int nBussinessType = 3;
+		bool ControlType[3] = {false,false,false}; 
+		const type_info &typeInfo = typeid(*pWnd);
+		string sTypeName = typeInfo.raw_name();
+		if (string::npos != sTypeName.find("CBusinessEdit"))
+		{
+			ControlType[Business_Edit] = true;
+			pBusinessControl = dynamic_cast<CBusinessEdit*>(pWnd);
+			if(!pBusinessControl) continue;
+			sBusinessField = pBusinessControl->m_sBusinessField;
+		}
+		else if (string::npos != sTypeName.find("CBusinessComboBox"))
+		{
+			ControlType[Business_ComboBox] = true;
+			pComboBoxControl = dynamic_cast<CBusinessComboBox*>(pWnd);
+			if(!pComboBoxControl) continue;
+			sBusinessField = pComboBoxControl->m_sBusinessField;
+		}
+		else if (string::npos != sTypeName.find("CBusinessCheckBox"))
+		{
+			ControlType[Business_CheckBox] = true;
+			pCheckBoxControl = dynamic_cast<CBusinessCheckBox*>(pWnd);
+			if(!pCheckBoxControl) continue;
+			sBusinessField = pCheckBoxControl->m_sBusinessField;
+		}
+
+		map<string,CFieldDesc*>::iterator itFieldDesc = mapTableName2FieldDesc.find(sBusinessField);
+		if(mapTableName2FieldDesc.end() == itFieldDesc) continue;
+
+		CFieldDesc* pFieldDesc = itFieldDesc->second;
+		if (!pFieldDesc) continue;
+
+		char szValue[512] = {0}; 
+		pWnd->GetWindowTextA(szValue,512);
+
+		char szSQL[512] = {0}; 
+		if (pFieldDesc->m_strDataType == "string")
+		{
+			sprintf_s(szSQL,512," %s = '%s' ",sBusinessField.c_str(),szValue);
+			if (pFieldDesc->m_strFieldName.find("FutureContractName") != string::npos)
+			{
+				if (!CTradeDayPrimaryData::Instance().m_bNeed2UpdateFutureContractName)
+				{
+					CTradeDayPrimaryData::Instance().m_strFutureContractName_LastTime = CTradeDayPrimaryData::Instance().m_strFutureContractName;
+					CTradeDayPrimaryData::Instance().m_strFutureContractName = szValue;
+					if (CTradeDayPrimaryData::Instance().m_strFutureContractName_LastTime != CTradeDayPrimaryData::Instance().m_strFutureContractName)
+					{
+						CTradeDayPrimaryData::Instance().m_bNeed2UpdateFutureContractName = true;
+						CTradeDayPrimaryData::Instance().m_nPlace2UpdateFutureContractName = Place2UpdateFutureContractName;
+					}
+				}
+			}
+		}
+		else if (pFieldDesc->m_strDataType == "float" )
+		{
+			sprintf_s(szSQL,512," %s = %s ", sBusinessField.c_str(), strcmp(szValue,"")?szValue:"0");
+		}
+		else if( pFieldDesc->m_strDataType == "integer" || pFieldDesc->m_strDataType == "int")
+		{
+			if (ControlType[Business_ComboBox])
+			{
+				int nValue = -1;
+				bool bFindValue = pComboBoxControl->GetValue(szValue,nValue);
+				sprintf_s(szSQL,512," %s = %d ", sBusinessField.c_str(), nValue);
+			}
+			else if (ControlType[Business_CheckBox])
+			{
+				int nValue = pCheckBoxControl->GetCheck();
+				sprintf_s(szSQL,512," %s = %d ", sBusinessField.c_str(), nValue);
+			}
+			else
+			{
+				sprintf_s(szSQL,512," %s = %s ", sBusinessField.c_str(), strcmp(szValue,"")?szValue:"0");
+			}
+		}
+
+		sSQL += szSQL;
+		if (m_mapBusiness2Control.size() != nUIControlCount)
+		{
+			sSQL += ",";
+		}
+		else
+		{
+			sSQL += " ";
+		}
+
+	}
+
+	sSQL += " where ";
+
+	vector<CFieldDesc*> vPrimaryKey;
+	CDBDataManager::Instance().GetPrimaryKey(m_sBusiness,vPrimaryKey);
+
+	string strPrimaryKeyClause = "";
+	for (int i = 0; i < vPrimaryKey.size();i++)
+	{
+		char sz[256] = {0};
+		CFieldDesc* pFieldDesc = vPrimaryKey[i];
+		if(!pFieldDesc) continue;
+
+		if (pFieldDesc->m_strFieldName.find("TradeDay") != string::npos)
+		{
+			sprintf_s(sz,256,"%s = '%s'",pFieldDesc->m_strFieldName.c_str(),CTradeDayPrimaryData::Instance().m_strTradeDay.c_str());
+		}
+		else if (pFieldDesc->m_strFieldName.find("FutureContractName") != string::npos)
+		{
+			sprintf_s(sz,256,"%s = '%s'",pFieldDesc->m_strFieldName.c_str(),CTradeDayPrimaryData::Instance().m_strFutureContractName_LastTime.c_str());
+		}
+
+		if (vPrimaryKey.size() - 1 != i)
+		{
+			strPrimaryKeyClause += sz;
+			strPrimaryKeyClause += " and ";
+		}
+		else
+		{
+			strPrimaryKeyClause += sz;
+		}	
+	}
+
+	sSQL += strPrimaryKeyClause;
+	CDBDataManager::Instance().Exec(sSQL);
+}
+
+void CDialogFutureContract_DailyTraceConflict::UpdateDB2UI(CDataSet& ds,int index)
+{
+	CRecord* pRecord = ds[0];
+	map<string,CFieldDesc*>& mapTableName2FieldDesc = CDBDataManager::Instance().GetTableMeta(m_sBusiness);
+	for(map<string,CWnd*>::iterator it = m_mapBusiness2Control.begin();
+		it != m_mapBusiness2Control.end();it++)
+	{
+		string sCaption = it->first;
+		CWnd* pWnd = it->second;
+		string sBusinessField = "";
+		//截至目前，有3中Business 字段类型
+		CBusinessEdit* pBusinessControl = NULL;
+		CBusinessComboBox* pComboBoxControl = NULL;
+		CBusinessCheckBox* pCheckBoxControl = NULL;
+
+		const int nBussinessType = 3;
+		bool ControlType[3] = {false,false,false}; 
+		const type_info &typeInfo = typeid(*pWnd);
+		string sTypeName = typeInfo.raw_name();
+		if (string::npos != sTypeName.find("CBusinessEdit"))
+		{
+			ControlType[Business_Edit] = true;
+			pBusinessControl = dynamic_cast<CBusinessEdit*>(pWnd);
+			if(!pBusinessControl) continue;
+			sBusinessField = pBusinessControl->m_sBusinessField;
+		}
+		else if (string::npos != sTypeName.find("CBusinessComboBox"))
+		{
+			ControlType[Business_ComboBox] = true;
+			pComboBoxControl = dynamic_cast<CBusinessComboBox*>(pWnd);
+			if(!pComboBoxControl) continue;
+			sBusinessField = pComboBoxControl->m_sBusinessField;
+		}
+		else if (string::npos != sTypeName.find("CBusinessCheckBox"))
+		{
+			ControlType[Business_CheckBox] = true;
+			pCheckBoxControl = dynamic_cast<CBusinessCheckBox*>(pWnd);
+			if(!pCheckBoxControl) continue;
+			sBusinessField = pCheckBoxControl->m_sBusinessField;
+		}
+
+
+		//根据FieldID 找到 CFieldDesc
+		map<string,CFieldDesc*>::iterator itFieldDesc = mapTableName2FieldDesc.find(sBusinessField);
+		if(mapTableName2FieldDesc.end() == itFieldDesc) continue;
+
+		CFieldDesc* pFieldDesc = itFieldDesc->second;
+		if (!pFieldDesc) continue;
+
+		//根据FieldDesc 找到 CField
+		CField* pField = pRecord->GetField(pFieldDesc->m_strFieldName);
+		if(!pField) return;
+
+		CString strValue = "";
+		const string strDataType = pFieldDesc->m_strDataType;
+		const string strDisplayType = pFieldDesc->GetAttributeString("DisplayType");
+		if (strDataType.find("string") != string::npos)
+		{
+			string sValue = pField->GetValueAsString();
+			strValue.Format(strDisplayType.c_str(),sValue.c_str());
+		}
+		else if (strDataType.find("int") != string::npos)
+		{
+			int nValue = pField->GetValueAsInt();
+			if (ControlType[Business_ComboBox])
+			{
+				string sMeaning = "";
+				bool bFindValue = pComboBoxControl->GetMeaning(nValue,sMeaning);
+				strValue = sMeaning.c_str();
+			}
+			else if (ControlType[Business_CheckBox])
+			{
+				pCheckBoxControl->SetCheck(nValue);
+				pCheckBoxControl->GetWindowTextA(strValue);
+			}
+			else
+			{
+				strValue.Format(strDisplayType.c_str(),nValue);
+			}	
+
+		}
+		else if (strDataType.find("float") != string::npos)
+		{
+			strValue.Format(strDisplayType.c_str(),pField->GetValueAsFloat());
+		}
+		else if (strDataType.find("double") != string::npos)
+		{
+			strValue.Format(strDisplayType.c_str(),pField->GetValueAsDouble());
+		}
+
+		pWnd->SetWindowTextA(strValue);
+	}
+}
