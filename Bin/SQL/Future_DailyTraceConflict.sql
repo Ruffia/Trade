900b29cafe3268a8841fce2cecddc2d98f19994b
Marco <n02022407@163.com>
2023-02-20 14:39:41 +0800

    修改表结构

--- Bin/SQL/Future_DailyTraceConflict.sql
+++ Bin/SQL/Future_DailyTraceConflict.sql
@@ -10,7 +10,7 @@
  Target Server Version : 3035005 (3.35.5)
  File Encoding         : 65001
 
- Date: 16/02/2023 08:38:30
+ Date: 20/02/2023 14:38:01
 */
 
 PRAGMA foreign_keys = false;
@@ -22,12 +22,9 @@ DROP TABLE IF EXISTS "Future_DailyTraceConflict";
 CREATE TABLE "Future_DailyTraceConflict" (
   "TradeDay" TEXT NOT NULL,
   "FutureContractName" TEXT NOT NULL,
-  "number" integer NOT NULL,
-  "TechnicalIndex" integer,
-  "IndexValue" integer,
-  "ConflictTechnicalIndex" integer,
-  "ConflictIndexValue" integer,
-  PRIMARY KEY ("TradeDay", "FutureContractName", "number")
+  "ConflictType" integer NOT NULL,
+  "DetailedDescription" text,
+  PRIMARY KEY ("TradeDay", "FutureContractName", "ConflictType")
 );
 
 PRAGMA foreign_keys = true;
